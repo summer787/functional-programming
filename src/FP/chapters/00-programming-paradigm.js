f1cdb9f2b3ba293312a11fa006ddf9dcc5643d04
leesy <leesy787@gmail.com>
2023-08-08 16:19:27 +0900

    수업 2 일차 - 캡슐화 - 비공개(private) - 객체 중심 설계 - HTML 요소를 반환하는 렌더와 HTML 문자열을 반환하는 렌더 - 렌더 -바인더이벤트 -함수 vs. 객체 지향 vs. 웹 컴포넌트 -각 프로그래밍 패러다임에서의 이벤트 감지 및 연결 처리 -함수형 프로그래밍 기본 원칙 - 하나의 기능에 집중 - 함수를 여러 개로 구성 -ESLint 구성 및 규칙 설정

--- src/FP/chapters/00-programming-paradigm.js
+++ src/FP/chapters/00-programming-paradigm.js
@@ -19,45 +19,24 @@ const courses = [
   },
 ];
 
-// console.log('원본 데이터\n', courses);
+const updateCourses = [];
 
-// ES2015(v6)
-// [전개구문(spread syntax)]을 사용하면 배열을 복사할 수 있다.
-// 얕은 복사 (shallow copy)
-const updateCourses = []; // [...courses];
-
-// 1. 과정 배열을 순환하여 각 과정 이름의 좌우 공백 제거
-// 명령형으로 프로그래밍 한다.
-// C, JAVA 문법
-// for문
 for (let i = 0, l = courses.length; i < l; i = i + 1) {
-  // 객체 복제는 어떻게???
-  // [전개구문(spread syntax)]을 사용한다.
-  // 얕은 복사 (shallow copy)
   const course = { ...courses[i] };
   course.name = course.name.trim();
-  updateCourses.push(course); // [{ ...}, { ... }]
+  updateCourses.push(course);
 }
 
-// 2. 과정 배열을 순환하여 각 과정 이름 대문자화
 for (let i = 0, l = updateCourses.length; i < l; ++i) {
   const course = updateCourses[i];
   course.name = course.name.toUpperCase();
 }
 
-// 3. 배열 원소의 `name` 속성의 공백을 밑줄(_)로 변경하는 기능 추가
-// 명령형 프로그래밍 방식으로
 for (let i = 0, l = updateCourses.length; i < l; ++i) {
   const course = updateCourses[i];
   course.name = course.name.replace(/\s+/g, '_');
 }
 
-// console.log('업데이트 데이터\n', updateCourses);
-
-// console.assert(
-//   !Object.is(courses, updateCourses),
-//   '🚨 courses와 updateCourses는 동일한 객체이다.'
-// );
 
 // --------------------------------------------------------------------------
 // 선언형 프로그래밍
@@ -73,105 +52,215 @@ const subjects = [
   },
 ];
 
-// console.log('원본 데이터\n', subjects);
-
-// 1. 객체 이름(name) 속성 좌우 공백 제거 함수 선언
 function toTrim(object) {
   const o = { ...object };
   o.name = o.name.trim();
   return o;
 }
 
-// console.log(toTrim(subjects[0]));
-// console.log(toTrim(subjects[1]));
-
-// 2. 객체 이름(name) 속성 대문자화 함수 선언
 function toUpperCase(object) {
   const o = { ...object };
   o.name = o.name.toUpperCase();
   return o;
 }
 
-// 3. 배열 원소의 `name` 속성의 공백을 밑줄(_)로 변경하는 기능 추가
-// 선언형 프로그래밍 방식으로
 function toUnderscore(object) {
   const o = { ...object };
   o.name = o.name.replace(/\s+/g, '_');
   return o;
 }
 
-// console.log(toUpperCase(subjects[0]));
-// console.log(toUpperCase(subjects[1]));
-
-// 3. 과목 이름 "좌우 공백 제거" → "대문자화" 후, 새로운 과목 배열 생성
-// ES5의 map()을 사용해야 한다.
-// - 조건 1. 새로운 배열 반환
-// - 조건 2. 배열 순환 후, 기능 처리(적용)
-
-/* const updateSubjects = subjects.map(subject => {
-  const copySubject = toTrim(subject);
-  return copySubject;
-}).map(subject=> {
-  const copySubject = toUpperCase(subject);
-  return copySubject;
-}) */
-
-const updateSubjects = subjects
-  // .map(toTrim)
-  // .map(toUpperCase)
-  .map(toUnderscore)
-  .map(toTrim);
-
-// console.log('업데이트 데이터\n', updateSubjects);
+const updateSubjects = subjects.map(toTrim).map(toUpperCase).map(toUnderscore);
 
 // --------------------------------------------------------------------------
 // JavaScript 프로그래밍 패러다임
 // → 함수(function)를 사용해 구현합니다.
 
-function createCountUpButton(container, potions = {}) {
-  if (!container || container.nodeType !== document.ELEMENT_NODE) {
-    throw new Error('container는 문서의 요소가 아닙니다.');
+function createCounterButton(element, { count = 0, step = 1, update = null } = {}) {
+  if (!element) {
+    throw new Error('element가 문서에 존재하지 않습니다.');
   }
 
-  let count = 0;
+  const setCount = (newCount) => {
+    count = newCount;
+    element.textContent = count;
+  }
 
-  const countUpButton = document.createElement('button');
+  element.addEventListener('click', () => {
+    setCount(count + step);
+    update?.(count);
+  });
 
-  const render = (newCount) => {
-    countUpButton.textContent = String(newCount);
-  };
+  setCount(count);
 
-  const handleCountUp = (e) => {
-    count += 1;
-    render(count);
-  };
-
-  countUpButton.setAttribute('type', 'button');
-  countUpButton.classList.add('CountUpButton');
-  countUpButton.addEventListener('click', handleCountUp);
-
-  render(count);
-
-  container.append(countUpButton);
+  return element;
 }
 
-const demoContainer = document.getElementById('demo');
+const counter = createCounterButton(
+  document.createElement('button'), 
+  {
+    count: 1,
+    update(count) {
+      document.querySelector('.functional').textContent = String(count);
+    }
+  }
+);
 
-createCountUpButton(demoContainer);
-createCountUpButton(demoContainer);
-createCountUpButton(demoContainer);
-createCountUpButton(demoContainer);
-createCountUpButton(demoContainer);
-createCountUpButton(demoContainer);
-createCountUpButton(demoContainer);
-createCountUpButton(demoContainer);
+document.getElementById('demo')?.append(counter);
+
 
 // --------------------------------------------------------------------------
 // JavaScript 프로그래밍 패러다임
 // → 클래스(class)를 사용해 구현합니다. (참고: https://mzl.la/3QrTKlF)
 
-class CountUpButton {}
+class CounterButton {
+  #element = null;
+  #config = {};
+  #updateCallback = null;
+  #clearIntervalId = 0;
+
+  static defaultOptions = {
+    count: 0,
+    step: 1,
+  }
+
+  constructor(element, props = {}) {
+    if (!element) {
+      throw new Error('element가 문서에 존재하지 않습니다.');
+    }
+
+    this.#element = element;
+    this.#init(props);
+  }
+
+  #init(props) {
+    this.setConfig(props);
+    this.#updateDOM();
+    this.#bindEvent();
+  }
+
+  #bindEvent() {
+    this.#element.addEventListener('click', () => {
+      this.setCount();
+      this.#updateCallback?.(this.#config.count);
+    });
+  }
+
+  #updateDOM() {
+    const { count } = this.#config;
+    this.#element.textContent = count;
+  }
+
+  setConfig(userConfig = {}) {
+    this.#config = { ...CounterButton.defaultOptions, ...userConfig };
+  }
+
+  setCount(newCount) {
+    const { count, step } = this.#config;
+
+    this.setConfig({
+      ...this.#config,
+      count: newCount ?? count + step,
+    });
+
+    this.#updateDOM();
+  }
+
+  update(callback) {
+    this.#updateCallback = callback;
+  }
+
+  play(fps = 1000 / 1) {
+    this.#clearIntervalId = setInterval(() => {
+      const { count, step } = this.#config;
+      this.setCount(count + step);
+      this.#updateDOM();
+    }, fps);
+  }
+
+  stop() {
+    clearInterval(this.#clearIntervalId);
+  }
+
+  mount(container) {
+    container.append(this.#element);
+  }
+}
+
+const counterButton = new CounterButton(
+  document.createElement('button'),
+  {
+    count: 2,
+    step: 2
+  }
+);
+
+counterButton.update((count) => {
+  document.querySelector('.object-oriented').textContent = String(count);
+})
+
+counterButton.mount(document.getElementById('demo'));
+
 
 // --------------------------------------------------------------------------
 // 웹 컴포넌트(Web Components) API
 // → 웹 컴포넌트를 사용해 구현합니다. (참고: https://mzl.la/3YjFdu9)
+
+class CounterButtonComponent extends HTMLElement {
+  #config = {
+    count: 0,
+    step: 1,
+  }
+
+  constructor() {
+    super();
+    this.#init();
+  }
+
+  #init() {
+    const userConfig = {
+      count: Number(this.getAttribute('count')),
+      step: Number(this.getAttribute('step')) || 1,
+    };
+
+    this.#config = { ...this.#config, ...userConfig };
+  }
+
+  #bindEvent(e) {
+    if (e.target.matches('button')) {
+      this.#setCount();
+      this.render();
+      // 참고: https://developer.mozilla.org/ko/docs/Web/Events/Creating_and_triggering_events
+      this.dispatchEvent(new CustomEvent('update', { detail: this.#config.count }));
+    }
+  }
+
+  #setCount() {
+    const { count, step } = this.#config;
+    this.#config.count = count + step;
+  }
+
+  connectedCallback() {
+    // console.log('connected');
+    this.render();
+    this.addEventListener('click', (e) => this.#bindEvent(e));
+  }
+
+  disconnectedCallback() {
+    // console.log('disconnected');
+    this.removeEventListener('click', (e) => this.#bindEvent(e));
+  }
+
+  render() {
+    const { count } = this.#config;
+    this.innerHTML = `<button type="button">${count}</button>`;
+  }
+}
+
+customElements.define('counter-button', CounterButtonComponent);
+
+const counterButtonEl = document.querySelector('counter-button');
+
+counterButtonEl.addEventListener('update', (e) => {
+  document.querySelector('.web-component').textContent = String(e.detail);
+});
